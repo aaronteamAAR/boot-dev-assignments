f05c63ac97a3475fe05457add7fce8b7fd5fba32
Aaron Chris <rooneychris83@gmail.com>
2022-07-15 14:56:18 +0100

    boot.dev assign

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-let inventory = ["Iron Breastplate", "Healing Potion", "Leather Scraps", "Shortsword"]
+let inventory = [
+  "Iron Breastplate",
+  "Healing Potion",
+  "Leather Scraps",
+  "Shortsword",
+  "Iron ore"
+];
 
-console.table(inventory)
+console.log(inventory[0] = "Iron Bar");
+
+
+playersId = [
+    "0-33",
+    "0-23",
+    "0-12"
+]
+console.log( playersId.IndexOf("0-33"))
+// Pushing value into an array with a loop
+for(let  i = 0; i < playersId.length; i++ ){
+}
+
+// Array Updates
+
+let inventory = [ 'Healing Potion', 'Iron Ore', 'Bread', 'Shortsword' ]
+console.log(inventory)
+
+// update second item here
+
+console.log(inventory)
